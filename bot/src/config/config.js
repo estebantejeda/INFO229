09c29790947d5f4c6d3ab9e4ae551dbe3ee0894b
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-01-10 21:38:10 -0300

    Agregado Dockerfile a bot

--- bot/src/config/config.js
+++ bot/src/config/config.js
@@ -2,8 +2,8 @@ const PORT = 3000;
 const TOKEN = "xoxb-1412458313702-1443060376240-fDvdxfgezGJHEf4Kqil4b6EK";
 const STSECRET = "d1760a47ac94cf17a1a7532b2ac7d8e0";
 const URL = 'mongodb://database/';
-const USERNAME = 'estebantejeda',
-const PASSWORD = 'testbot01'
+const USERNAME = 'estebantejeda';
+const PASSWORD = 'testbot01';
 
 module.exports = {
     PORT,
