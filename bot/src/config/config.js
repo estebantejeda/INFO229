d11db32cfa027f30416c2f57120f3a45b27104da
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-01-10 23:21:27 -0300

    Eliminada las variables de entorno

--- bot/src/config/config.js
+++ bot/src/config/config.js
@@ -1,9 +1,9 @@
 const PORT = 3000;
-const TOKEN = "xoxb-1412458313702-1443060376240-fDvdxfgezGJHEf4Kqil4b6EK";
-const STSECRET = "d1760a47ac94cf17a1a7532b2ac7d8e0";
+const TOKEN = "";
+const STSECRET = "";
 const URL = 'mongodb://database/';
-const USERNAME = 'estebantejeda';
-const PASSWORD = 'testbot01';
+const USERNAME = '';
+const PASSWORD = '';
 
 module.exports = {
     PORT,
