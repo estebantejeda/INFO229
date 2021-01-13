39fd7de97272f4c1b47f1b3686bd83f762f63bc6
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-01-13 18:52:21 -0300

    Borrado archivos inncesearios

--- bot/src/server.js
+++
@@ -1,9 +0,0 @@
-const {PORT} = require('./config/config');
-
-const slackEvents = require('./bots/task').slackEvents;
-
-slackEvents.start(PORT).then(() => {
-    console.log("Conectado en puerto 3000");
-});
-
-slackEvents.on('error', console.error);
